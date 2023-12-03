d99be99b6136d01e92a40f61ad0c75f02a91b58a
livingcodes <case.joyce@gmail.com>
2023-12-03 16:22:59 -0600

    day 1 puz 1

---
+++ Aoc/Test.cs
@@ -0,0 +1,8 @@
+﻿namespace Aoc;
+public class Test
+{
+  protected void log(str txt) => 
+    System.Diagnostics.Debug.WriteLine(txt);
+  protected void chk(bln cond) => 
+    Assert.IsTrue(cond);
+}
